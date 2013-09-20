dea68388a7dd9044ef027d235c18c60895bdb3ef
gtudan <Gregor.Tudan@cofinpro.de>
2013-09-20 15:45:07 +0200

    use reader and writer in same class

--- src/main/java/batch/ProductCatalogDao.java
+++ src/main/java/batch/ProductCatalogDao.java
@@ -3,14 +3,16 @@ package batch;
 import model.ProductItem;
 
 import javax.batch.api.chunk.ItemReader;
+import javax.batch.api.chunk.ItemWriter;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * @author Gregor Tudan
  */
-@Named("ProductCatalogReader")
-public class ProductCatalogReader implements ItemReader {
+@Named("ProductCatalogDao")
+public class ProductCatalogDAO implements ItemReader, ItemWriter{
 	@Override
 	public void open(Serializable serializable) throws Exception {
 		//To change body of implemented methods use File | Settings | File Templates.
@@ -18,6 +20,11 @@ public class ProductCatalogReader implements ItemReader {
 
 	@Override
 	public void close() throws Exception {
+		//To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public void writeItems(List<Object> objects) throws Exception {
 		//To change body of implemented methods use File | Settings | File Templates.
 	}
 
