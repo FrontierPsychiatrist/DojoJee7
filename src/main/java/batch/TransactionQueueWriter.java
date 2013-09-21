c0f349b87f95c2ebe9be62d7703230cdd5807d34
csobotta <christopher.sobotta@cofinpro.de>
2013-09-21 15:32:51 +0200

    Writer

--- src/main/java/batch/TransactionQueueWriter.java
+++ src/main/java/batch/TransactionQueueWriter.java
@@ -11,32 +11,45 @@ import javax.inject.Named;
 import javax.jms.JMSContext;
 import javax.jms.Queue;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import cdi.ProcessingCompleteEvent;
+import domain.TransactionReport;
+import domain.TransactionResult;
+
 @Named("TransactionQueueWriter")
 public class TransactionQueueWriter implements ItemWriter{
-
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
 	@Inject
 	private JMSContext context;
 	
 	@Resource(lookup = "java:global/jms/taskQueue")
 	private Queue queue;
 	
+	@Inject 
+	private TransactionReport transactionReport;
+	
 	@Inject
 	private Event<ProcessingCompleteEvent> event;
 	
 	@Override
 	public void open(Serializable checkpoint) throws Exception {
-		event.
+		log.info("Fire Event: "+ transactionReport.toString());
+		event.fire(new ProcessingCompleteEvent(transactionReport));
 	}
 
 	@Override
 	public void close() throws Exception {
-	
-		
+		//
 	}
 
 	@Override
 	public void writeItems(List<Object> items) throws Exception {
-		
+		for(Object transactionResult : items){
+			this.transactionReport.getTransactionResults().add((TransactionResult) transactionResult);
+		}
 	}
 
 	@Override
--- src/main/java/domain/TransactionReport.java
+++ src/main/java/domain/TransactionReport.java
@@ -1,9 +1,22 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionReport {
 	private List<TransactionResult> transactionResults;
+
+	public List<TransactionResult> getTransactionResults() {
+		if(transactionResults == null){
+			transactionResults = new ArrayList<>();
+		}
+		return transactionResults;
+	}
+
+	public void setTransactionResults(List<TransactionResult> transactionResults) {
+		this.transactionResults = transactionResults;
+	}
+	
 	
 	
 }
